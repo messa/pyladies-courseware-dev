9de9105d3034360cfc52e24c35dfd55b21a46e9f
Petr Messner <petr.messner@gmail.com>
2020-01-23 17:23:20 +0100

    frontend: import codemirror CSS from node_modules instead of CDN <link>

---
+++ frontend/pages/_app.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/theme/solarized.css'
+
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
