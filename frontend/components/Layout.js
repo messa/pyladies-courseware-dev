52f41503360a3ac77e4d2f6298aaa2cfaec82d5c
Petr Messner <petr.messner@gmail.com>
2021-04-03 17:56:38 +0200

    frontend: update semantic-ui 2.3.3 -> 2.4.2

--- frontend/components/Layout.js
+++ frontend/components/Layout.js
@@ -8,7 +8,7 @@ export default ({ children, user, activeItem, width }) => (
       <title>Pyladies Courseware</title>
       <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, shrink-to-fit=no" />
       <link rel="stylesheet" href="/static/font-lato/lato.css" />
-      <link rel="stylesheet" href="/static/semantic-ui/semantic-2.3.3.no-font.css" />
+      <link rel="stylesheet" href="/static/semantic-ui/semantic-2.4.2.no-font.css" />
       <link rel="stylesheet" href="/static/main2.css" />
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.40.0/codemirror.min.css"
         integrity="sha256-I8NyGs4wjbMuBSUE40o55W6k6P7tu/7G28/JGUUYCIs=" crossOrigin="anonymous" />
